b97ed6f73772ba0b3c63ed60a3abd1fe46e78cf4
yashchuri53 <107393908+yashchuri53@users.noreply.github.com>
2022-08-12 09:50:47 +0530

    Add files via upload

---
+++ M=20/src/com/tns/core/Employee.java
@@ -0,0 +1,24 @@
+package com.tns.core;
+
+public class Employee {
+	private int age;
+	private String name;
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+
+}
